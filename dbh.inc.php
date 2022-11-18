d7aa5eb5aa2cb72ea6ceabe381038254d1918103
Lhtlakshan <91305073+Lhtlakshan@users.noreply.github.com>
2022-11-18 08:35:21 +0530

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,16 @@
+<?php
+   
+   $host = "localhost";
+   $dbname = "be_care";
+   $username = "root";
+   $password = "";
+
+   $mysqli = new mysqli($host ,$username ,$password , $dbname);
+
+ if($mysqli->connect_errno){
+    die("Connection error: " . $mysql->connect_error);
+ } 
+
+ return $mysqli;
+ 
+?>
---
+++ main_input.php
@@ -0,0 +1,43 @@
+<?php include("include/signup_header.php") ?>
+
+
+    <div class="mid">
+        <div class="title">Login</div> 
+        <div class="center_wrapper_mid">
+            <div class="left2">
+                
+            </div>
+            <div class="center">
+                <form action="login.php" class="form" method = "post">
+                    <div class = "input_main">
+
+                        <label for="user_type">User type</label> <br>
+
+                        <select name="user_type" id="user_type">
+                            <option value="patient">Patient</option>
+                            <option value="Doctor">Doctor</option>
+                            <option value="counsellor">Consellor</option>
+                            <option value="nutritionist">Nutritionist</option>
+                            <option value="meditation_instructor">meditation_instructor</option>
+                            <option value="admin">Admin</option>
+                        </select> <br><br>
+                       
+                        <button type="submit" id="Signup_btn2">Submit</button>
+                     
+                        
+                    </div>
+                  
+                </form>
+                    
+            </div>
+            <div class="right2">
+             </div>
+        </div>
+    </div>
+
+    
+     
+     </div>
+    </div>
+
+<?php include("include/signup_footer.php") ?>
