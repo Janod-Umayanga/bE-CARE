6866a10c5ddaccbefbd8d5fd4c733dce39c420b8
Janod Umayanga <JanodUmayanga@gmail.com>
2023-01-16 23:11:29 +0530

    Doctor timeslots UI modifications

--- app/views/patients/v_doctor_appointments.php
+++ app/views/patients/v_doctor_appointments.php
@@ -4,6 +4,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/c4a594ff55.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/f1513ae29e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style2.css">
+    <script defer src="<?php echo URLROOT; ?>/js/script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <?php require APPROOT.'/views/inc/components/header1.php'; ?>
+
+    <div class="time-slot-main-picture-container">
+        <div class="tittle">
+            <i class="fa-solid fa-stopwatch"></i>
+            <h1>Keep in touch with Your<br>Appointments!</h1>
+        </div>
+    </div>
+
+    <section class="pharmacy-card-container theme">
+        <div class="pharmacy-cards-topic">
+            <span class="line"></span>
+            <h2>Appointments</h2>
+        </div>
+        <div class="time-slot-main-container" id="to-be-show-more">
+            <div class="time-slot-sub">
+            <?php foreach($data['appointments'] as $appointment): ?>
+                <?php if($appointment->date > $data['currentDate'] || ($appointment->date == $data['currentDate'] && $appointment->time >= $data['currentTime'])): ?>
+                    <div class="card">
+                        <div class="top">
+                            <h1>Dr. <?php echo $appointment->first_name ?> <?php echo $appointment->last_name ?></h1>
+                            <p><?php  echo $appointment->address ?></p>
+                        </div>
+                        <div class="bottom">
+                            <p><i class="fa-solid fa-clock"></i> <?php  echo $appointment->date ?></p>
+                            <p><i class="fa-solid fa-clock"></i> <?php  echo $appointment->time ?></p>
+                        </div>
+                        <div class="top-box"></div>
+                        <div class="bottom-box"></div>
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            </div>
+        </div>
+        <button id="show-button" class="show-button" onclick="showMore()"><span id="show-text">Show More</span><i class="fa-solid fa-angle-down" id="icon-more-orless"></i></button>
+    </section>
+
+    <?php require APPROOT.'/views/inc/components/footer1.php'; ?>
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <title>View Doctor Appointments</title>
 </head>
@@ -40,4 +97,4 @@
 <?php require APPROOT.'/views/inc/components/footer.php'; ?>
 
 </body>
-</html>
+</html> -->
--- app/views/patients/v_doctor_timeslots.php
+++ app/views/patients/v_doctor_timeslots.php
@@ -4,39 +4,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
-    <title>Doctor Timeslots</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/c4a594ff55.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/f1513ae29e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style2.css">
+    <script defer src="<?php echo URLROOT; ?>/js/script.js"></script>
+    <title>Document</title>
 </head>
 <body>
+    <?php require APPROOT.'/views/inc/components/header1.php'; ?>
 
-<?php require APPROOT.'/views/inc/components/header.php'; ?>
+    <div class="time-slot-main-picture-container">
+        <div class="tittle">
+            <i class="fa-solid fa-stopwatch"></i>
+            <h1>Find the best time<br>you need!</h1>
+        </div>
+    </div>
 
-    <section class="doctor-cards">
-        <div class="left">
-            <?php foreach($data['timeslots'] as $timeslot): ?>
-            <div class="card">
-                <div class="card-left">
-                     <p>Day</p>
-                </div>
-                <div class="card-right">
-                    <div>
-                        <h3><?php  echo $timeslot->day ?></h3>
-                        <p><?php  echo $timeslot->starting_time ?> - <?php  echo $timeslot->ending_time ?></p>
-                        <p>At <?php  echo $timeslot->address ?></p>
-                        <p>Channeling fee: Rs.<?php  echo $timeslot->fee ?></p>
+    <section class="pharmacy-card-container theme">
+        <div class="pharmacy-cards-topic">
+            <span class="line"></span>
+            <h2>Doctor's Available Timeslots</h2>
+        </div>
+        <div class="time-slot-main-container" id="to-be-show-more">
+            <div class="time-slot-sub">
+                <?php foreach($data['timeslots'] as $timeslot): ?>
+                <div class="card">
+                    <div class="top">
+                        <h1><?php  echo $timeslot->channeling_day ?></h1>
+                        <p><?php  echo $timeslot->address ?></p>
                     </div>
-                    <a href="">Channel</a>
+                    <div class="bottom">
+                        <p><i class="fa-solid fa-clock"></i> <?php  echo $timeslot->day ?></p>
+                        <p><i class="fa-solid fa-clock"></i> <?php  echo $timeslot->starting_time ?> - <?php  echo $timeslot->ending_time ?></p>
+                        <p><i class="fa-solid fa-phone-volume"></i><?php  echo $timeslot->contact_number ?></p>
+                        <p><i class="fa-solid fa-circle-dollar-to-slot"></i>Rs. <?php  echo $timeslot->fee ?></p>
+                    </div>
+                    <div class="top-box"></div>
+                    <div class="bottom-box"></div>
+                    <button class="main-button">Channel</button>
                 </div>
+                <?php endforeach; ?>
             </div>
-            <?php endforeach; ?>
         </div>
-
-        <div class="right">
-            <img src="../public/img/doctor-cards.png" alt="">
-        </div>
+        <button id="show-button" class="show-button" onclick="showMore()"><span id="show-text">Show More</span><i class="fa-solid fa-angle-down" id="icon-more-orless"></i></button>
     </section>
 
-<?php require APPROOT.'/views/inc/components/footer.php'; ?>
-
+    <?php require APPROOT.'/views/inc/components/footer1.php'; ?>
 </body>
 </html>
