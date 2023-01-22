a25101933f87ac9e59cc29f7b4e12e52bb5cd77b
dana528 <108984326+dana528@users.noreply.github.com>
2023-01-22 19:06:05 +0530

    Add files via upload

---
+++ danu_MVC/public/danu_MVC/public/index.php
@@ -0,0 +1,7 @@
+<?php
+    # redirect to bootloader.php
+    require_once '../app/bootloader.php';   # redirect to bootloader.php
+
+    $init = new Core; //instantiate the Core class in index file 
+ 
+?>
