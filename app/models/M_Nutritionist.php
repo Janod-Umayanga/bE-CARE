3bd47a69b394a5e74319c03d3e796bb12b3c1798
dana528 <108984326+dana528@users.noreply.github.com>
2023-02-05 21:01:35 +0530

    Update M_Nutritionist.php

--- app/models/M_Nutritionist.php
+++ app/models/M_Nutritionist.php
@@ -28,6 +28,70 @@
             
             return $this->db->single();
         }
+        
+          // Find nutritionist by email
+        public function findNutritionistByEmail($email) {
+            $this->db->query('SELECT * FROM nutritionist WHERE email = :email');
+            $this->db->bind(':email', $email);
+
+            $row = $this->db->single();
+
+            if($this->db->rowCount() > 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        
+        // Get all requested diet plans of logged nutritionist
+        public function getAllRequestedDietPlanDetails($nutritionist_id){
+        $this->db->query('SELECT * FROM request_diet_plan inner join nutritionist on 
+        nutritionist.nutritionist_id =request_diet_plan.nutritionist_id
+        WHERE nutritionist.nutritionist_id = :nutritionist_id');
+        $this->db->bind(':nutritionist_id',$nutritionist_id);  
+
+        return $this->db->resultSet();
+        }
+        
+         // Get all requested diet plans of logged nutritionist(more details)
+         public function getAllRequestedDietPlanMoreDetails($nutritionist_id){
+            $this->db->query('SELECT * FROM request_diet_plan inner join nutritionist on 
+            nutritionist.nutritionist_id =request_diet_plan.nutritionist_id
+            WHERE nutritionist.nutritionist_id = :nutritionist_id');
+            $this->db->bind(':nutritionist_id',$nutritionist_id);  
+    
+            return $this->db->resultSet();
+            }
+
+        // get all issued diet plans history of nutritionist
+        public function viewHistoryPage($nutritionist_id){
+            $this->db->query('SELECT * FROM diet_plan
+            WHERE nutritionist_id = :nutritionist_id');
+            $this->db->bind(':nutritionist_id',$nutritionist_id);  
+    
+            return $this->db->single();
+         }
+             
+        // Login nutritionist
+        public function login($email, $password) {
+            $this->db->query('SELECT * FROM nutritionist WHERE email = :email');
+            $this->db->bind(':email', $email);
+
+            $row = $this->db->single();
+
+            // Verify password
+            $hashed_pw = $row->password;
+            if(password_verify($password, $hashed_pw)) {
+                return $row;
+            }
+            else {
+                return false;
+            }
+        }
+        
+        
+        
 
         
     }
