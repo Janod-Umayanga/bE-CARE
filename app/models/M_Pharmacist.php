b00659c53be38fba85eeb49bd0656f13f2cb0bb7
dana528 <108984326+dana528@users.noreply.github.com>
2023-02-09 15:57:58 +0530

    Update M_Pharmacist.php

--- app/models/M_Pharmacist.php
+++ app/models/M_Pharmacist.php
@@ -62,10 +62,39 @@
 
         // get all orders details of Pharmacist
         public function getAllOrderDetails($pharmacist_id){
-            $this->db->query('SELECT * FROM  WHERE pharmacist_id= :pharmacist_id');
+            $this->db->query('SELECT * FROM order_request INNER JOIN pharmacist
+            ON order_request.pharmacist_id = pharmacist.pharmacist_id 
+            WHERE pharmacist.pharmacist_id= :pharmacist_id');
             $this->db->bind(':pharmacist_id',$pharmacist_id);  
     
-             return $this->db->resultSet();
+           /* $row= $this->db->single();
+    
+            if($this->db->rowCount() >0){
+                 return true;
+            }else{
+                 return false;
+            }*/
+            return $this->db->resultSet();
+        }
+
+        // update
+        
+        // Update pharmacist details
+        public function update($data, $pharmacist_id) {
+            $this->db->query('UPDATE patient SET first_name = :first_name, last_name = :last_name, nic = :nic, contact_number = :contact_number, gender = :gender WHERE patient_id = :patient_id');
+            $this->db->bind(':first_name', $data['fname']);
+            $this->db->bind(':last_name', $data['lname']);
+            $this->db->bind(':nic', $data['nic']);
+            $this->db->bind(':contact_number', $data['cnumber']);
+            $this->db->bind(':gender', $data['gender']);
+            $this->db->bind(':patient_id', $pharmacist_id);
+
+            if($this->db->execute()) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
   
